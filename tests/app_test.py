7be07ea32c5e897414c47cac5905cf20bc45e173
awhipp <alexruns4@gmail.com>
2024-05-07 17:16:58 -0400

    feat: :sparkles: Initial github CI and testing

---
+++ tests/app_test.py
@@ -0,0 +1,11 @@
+"""Application test for the flask app"""
+
+from sherlock.app import app
+
+def test_home_route():
+    """Test the home route."""
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+        assert response.data == b'Hello, World!'
+
